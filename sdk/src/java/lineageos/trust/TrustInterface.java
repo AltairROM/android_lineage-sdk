1ec21c1bae34069c4785f237a5c7cf1ccf2f2809
Martin J. Pollard <martin0566@gmail.com>
2019-09-14 13:03:36 -0400

    Trust: remove build signature check

--- sdk/src/java/lineageos/trust/TrustInterface.java
+++ sdk/src/java/lineageos/trust/TrustInterface.java
@@ -138,7 +138,7 @@ public class TrustInterface {
     public static final int TRUST_WARN_PUBLIC_KEY = 1 << 2;
 
     /**
-     * Max / default value for warnings status
+     * Max value for warnings status
      *
      * Includes all the TRUST_WARN_
      *
@@ -148,6 +148,15 @@ public class TrustInterface {
     public static final int TRUST_WARN_MAX_VALUE =
             TRUST_WARN_SELINUX |
             TRUST_WARN_PUBLIC_KEY;
+
+    /**
+     * Default value for warnings status
+     *
+     * @see #postNotificationForFeature
+     * @hide
+     */
+    public static final int TRUST_WARN_DEF_VALUE =
+            TRUST_WARN_SELINUX;
 
     private static final String TAG = "TrustInterface";
 
