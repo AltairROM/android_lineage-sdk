12cecb70bc7352e7a187e8eb1898c4e3e3f73e53
rohan <purohit.rohan@gmail.com>
2020-03-22 14:47:47 +0530

    [Nav] Introduce navigation bar enhancements
    This is a squash of the following crDroid commits:
    
        Add more device key actions
    
            - Torch
            - Screenshot
            - Volume
            - Clear All Notifications
            - Expand Notifications
            - QS panel
            - Ringer modes
    
            Change-Id: I410f75b9429b90bfe8985dd44558a1194abd6c8c
            Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
            Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        Set long press on back default to kill app
    
            Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        sdk: Add customization for double tap recents key
    
            Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    A few additional, Altair-specific enhancements were also added.

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -374,7 +374,7 @@ public final class LineageSettings {
             new InclusiveIntegerRangeValidator(0, 86400);
 
     private static final Validator sHardwareKeyActionvalidator =
-            new InclusiveIntegerRangeValidator(0, 11);
+            new InclusiveIntegerRangeValidator(0, 18);
 
     private static final Validator sAlwaysTrueValidator = new Validator() {
         @Override
@@ -1090,6 +1090,13 @@ public final class LineageSettings {
          * 9 - Toggle split screen
          * 10 - Kill foreground app
          * 11 - Play/pause music
+         * 12 - Torch
+         * 13 - Screenshot
+         * 14 - Volume Panel
+         * 15 - Clear All Notifications
+         * 16 - Notifications
+         * 17 - Toggle Qs Panel
+         * 18 - Ringer Modes
          */
         public static final String KEY_HOME_LONG_PRESS_ACTION = "key_home_long_press_action";
 
@@ -1218,6 +1225,16 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator KEY_APP_SWITCH_ACTION_VALIDATOR =
+                sHardwareKeyActionvalidator;
+
+        /**
+         * Action to perform when the app switch key is double-tapped. (Default is 8)
+         * (See KEY_HOME_LONG_PRESS_ACTION for valid values)
+         */
+        public static final String KEY_APP_SWITCH_DOUBLE_TAP_ACTION = "key_app_switch_double_tap_action";
+
+        /** @hide */
+        public static final Validator KEY_APP_SWITCH_DOUBLE_TAP_ACTION_VALIDATOR =
                 sHardwareKeyActionvalidator;
 
         /**
@@ -2162,6 +2179,8 @@ public final class LineageSettings {
             VALIDATORS.put(KEY_APP_SWITCH_ACTION, KEY_APP_SWITCH_ACTION_VALIDATOR);
             VALIDATORS.put(KEY_APP_SWITCH_LONG_PRESS_ACTION,
                     KEY_APP_SWITCH_LONG_PRESS_ACTION_VALIDATOR);
+            VALIDATORS.put(KEY_APP_SWITCH_DOUBLE_TAP_ACTION,
+                    KEY_APP_SWITCH_DOUBLE_TAP_ACTION_VALIDATOR);
             VALIDATORS.put(KEY_EDGE_LONG_SWIPE_ACTION, KEY_EDGE_LONG_SWIPE_ACTION_VALIDATOR);
             VALIDATORS.put(HOME_WAKE_SCREEN, HOME_WAKE_SCREEN_VALIDATOR);
             VALIDATORS.put(ASSIST_WAKE_SCREEN, ASSIST_WAKE_SCREEN_VALIDATOR);
