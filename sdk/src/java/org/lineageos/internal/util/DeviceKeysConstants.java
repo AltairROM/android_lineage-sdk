f995005260e77014b642cbcfa5ded86d42e5e3ae
rohan <purohit.rohan@gmail.com>
2020-03-22 14:47:47 +0530

    [Nav] Introduce navigation bar enhancements
    This is a squash of the following crDroid commits:
    
        Add more device key actions
    
            - Torch
            - Screenshot
            - Volume
            - Clear All Notifications
            - Expand Notifications
            - QS panel
            - Ringer modes
    
            Change-Id: I410f75b9429b90bfe8985dd44558a1194abd6c8c
            Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
            Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        Set long press on back default to kill app
    
            Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        sdk: Add customization for double tap recents key
    
            Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    A few additional, Altair-specific enhancements were also added.

--- sdk/src/java/org/lineageos/internal/util/DeviceKeysConstants.java
+++ sdk/src/java/org/lineageos/internal/util/DeviceKeysConstants.java
@@ -26,7 +26,14 @@ public class DeviceKeysConstants {
         LAST_APP,
         SPLIT_SCREEN,
         KILL_APP,
-        PLAY_PAUSE_MUSIC;
+        PLAY_PAUSE_MUSIC,
+        TORCH,
+        SCREENSHOT,
+        VOLUME_PANEL,
+        CLEAR_ALL_NOTIFICATIONS,
+        NOTIFICATIONS,
+        QS_PANEL,
+        RINGER_MODES;
 
         public static Action fromIntSafe(int id) {
             if (id < NOTHING.ordinal() || id > Action.values().length) {
@@ -52,4 +59,3 @@ public class DeviceKeysConstants {
     public static final int KEY_MASK_CAMERA = 0x20;
     public static final int KEY_MASK_VOLUME = 0x40;
 }
-
